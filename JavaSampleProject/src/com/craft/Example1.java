b4f58197b81dd1a96fb7505a13d01dc7cba55657
Samgoe <115717449+Samgoe@users.noreply.github.com>
2022-11-17 19:12:41 -0500

    commited from eclipse

---
+++ JavaSampleProject/src/com/craft/Example1.java
@@ -0,0 +1,13 @@
+package com.craft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("print something");
+	System.out.println("print samson"	);
+	System.out.println("print samson geses");
+	}
+
+}
